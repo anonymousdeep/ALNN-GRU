9f7bca65766554980316d68780154cb09b0c92f7
anonymousdeep <nzamba.bignoumba@taltech.ee>
2022-06-02 13:50:06 +0300

    add loss_function file

---
+++ loss_functions.py
@@ -0,0 +1,31 @@
+import keras.backend as K
+import tensorflow as tf
+
+    
+# Focal loss
+def binary_focal_loss(gamma=2., alpha=.15):
+
+    def binary_focal_loss_fixed(y_true, y_pred):
+       
+        y_true = tf.cast(y_true, tf.float32)
+        # Define epsilon so that the back-propagation will not result in NaN for 0 divisor case
+        epsilon = K.epsilon()
+        # Add the epsilon to prediction value
+        # y_pred = y_pred + epsilon
+        # Clip the prediciton value
+        y_pred = K.clip(y_pred, epsilon, 1.0 - epsilon)
+        # Calculate p_t
+        p_t = tf.where(K.equal(y_true, 1), y_pred, 1 - y_pred)
+        # Calculate alpha_t
+        alpha_factor = K.ones_like(y_true) * alpha
+        alpha_t = tf.where(K.equal(y_true, 1), alpha_factor, 1 - alpha_factor)
+        # Calculate cross entropy
+        cross_entropy = -K.log(p_t)
+        weight = alpha_t * K.pow((1 - p_t), gamma)
+        # Calculate focal loss
+        loss = weight * cross_entropy
+        # Sum the losses in mini_batch
+        loss = K.mean(K.sum(loss, axis=1))
+        return loss
+
+    return binary_focal_loss_fixed
